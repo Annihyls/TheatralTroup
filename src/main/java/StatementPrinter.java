f0dd70de939183b4d28ae108617c3bf60688ac90
GIMBERT Vincent <vincent.gimbert@yahoo.fr>
2023-10-10 15:53:17 +0200

    change string switch to static variable to make easier to modify in the future

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -2,7 +2,9 @@ import java.text.NumberFormat;
 import java.util.*;
 
 public class StatementPrinter {
-
+  public static final String TRAGEDY = "tragedy";
+  public static final String COMEDY = "comedy";
+  
   public String print(Invoice invoice, HashMap<String, Play> plays) {
     int totalAmount = 0;
     int volumeCredits = 0;
@@ -15,13 +17,13 @@ public class StatementPrinter {
       int thisAmount = 0;
 
       switch (play.type) {
-        case "tragedy":
+        case TRAGEDY:
           thisAmount = 40000;
           if (perf.audience > 30) {
             thisAmount += 1000 * (perf.audience - 30);
           }
           break;
-        case "comedy":
+        case COMEDY:
           thisAmount = 30000;
           if (perf.audience > 20) {
             thisAmount += 10000 + 500 * (perf.audience - 20);
@@ -35,7 +37,7 @@ public class StatementPrinter {
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+      if (COMEDY.equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
       // print line for this order
       result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
