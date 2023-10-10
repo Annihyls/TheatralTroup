67e9b872035aca452af0732959bfa42d221e844c
GIMBERT Vincent <vincent.gimbert@yahoo.fr>
2023-10-10 17:09:18 +0200

    add functions (always to make the code pretty)

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -19,54 +19,60 @@ public class StatementPrinter {
     this.volumeCredits = 0;
   }
   public String print() {
-    this.sb.append("Statement for ");
-    this.sb.append(this.invoice.customer);
-    this.sb.append("\n");
-
+    printClient();
     for (Performance perf : this.invoice.performances) {
       Play play = perf.play;
-      float thisAmount = 0;
-
+      float amountForThisPlay = 0;
       switch (play.type) {
         case TRAGEDY:
-          thisAmount = 400;
+          amountForThisPlay = 400;
           if (perf.audience > 30) {
-            thisAmount += 10 * (perf.audience - 30);
+            amountForThisPlay += 10 * (perf.audience - 30);
           }
           break;
         case COMEDY:
-          thisAmount = 300;
+          amountForThisPlay = 300;
           if (perf.audience > 20) {
-            thisAmount += 100 + 5 * (perf.audience - 20);
+            amountForThisPlay += 100 + 5 * (perf.audience - 20);
           }
-          thisAmount += 3 * perf.audience;
+          amountForThisPlay += 3 * perf.audience;
           break;
         default:
           throw new Error("unknown type: ${play.type}");
       }
-
       // add volume credits
       this.volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
       if (COMEDY.equals(play.type)) this.volumeCredits += Math.floor(perf.audience / 5);
 
-      // print line for this order
-      this.sb.append("  ");
-      this.sb.append(play.name);
-      this.sb.append(": ");
-      this.sb.append(this.frmt.format(thisAmount));
-      this.sb.append(" (");
-      this.sb.append(perf.audience);
-      this.sb.append(" seats)\n");
-      this.totalAmount += thisAmount;
+      printPlayAndPerfAudience(play, perf, amountForThisPlay);
+      this.totalAmount += amountForThisPlay;
     }
+    printTotalAmountAndCredits();
+    return this.sb.toString();
+  }
+
+  private void printClient() {
+    this.sb.append("Statement for ");
+    this.sb.append(this.invoice.customer);
+    this.sb.append("\n");
+  }
+  private void printPlayAndPerfAudience(Play play, Performance perf, float amountForThisPlay) {
+    this.sb.append("  ");
+    this.sb.append(play.name);
+    this.sb.append(": ");
+    this.sb.append(this.frmt.format(amountForThisPlay));
+    this.sb.append(" (");
+    this.sb.append(perf.audience);
+    this.sb.append(" seats)\n");
+  }
+
+  private void printTotalAmountAndCredits() {
     this.sb.append("Amount owed is ");
     this.sb.append(this.frmt.format(this.totalAmount));
     this.sb.append("\n");
     this.sb.append("You earned ");
     this.sb.append(volumeCredits);
     this.sb.append(" credits\n");
-    return this.sb.toString();
   }
-
 }
